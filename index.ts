5475dfecc1f5531bbf60775480e640e0d02e07a8
elwood-berry <eberry@anterasoftware.com>
2021-08-19 15:29:39 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 // Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const myName: string = 'Elwood Steven Berry'; //  We expect the variable, 'myName', to always be a string.
+
+// --
+const couldBeAnything: any = undefined;
+const iAmANumber: number = 100; // "-1000", "10.00"
+const yes: boolean = true;
+const no: boolean = false;
+const nothingHere: null = null;
+const noDefinition: undefined = undefined;
+// --
+const notSure: any = undefined;
+const appDiv: HTMLElement = document.getElementById('app'); // The div
+
+appDiv.innerHTML = `
+I was talking to <strong>${myName}</strong>
+`;
+
+console.log('Elwood Is Here');
